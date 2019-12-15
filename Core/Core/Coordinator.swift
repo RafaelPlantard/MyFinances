98107ae32b3000aa31ca365fad5e3ceb1390a4d8
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2019-12-15 01:52:36 -0300

    Add Core.framework to project

---
+++ Core/Core/Coordinator.swift
@@ -0,0 +1,11 @@
+//
+//  Coordinator.swift
+//  Core
+//
+//  Created by Rafael Ferreira on 12/15/19.
+//  Copyright © 2019 Swift Yah. All rights reserved.
+//
+
+protocol Coordinator {
+    func start()
+}
---
+++ Core/Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Rafael Ferreira on 12/15/19.
+//  Copyright © 2019 Swift Yah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
