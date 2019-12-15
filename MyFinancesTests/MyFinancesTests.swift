4e2628139ca1be047a2eb218d72a6cc3960e0ddc
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2019-12-15 00:48:34 -0300

    Initial Commit

---
+++ MyFinancesTests/MyFinancesTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyFinancesTests.swift
+//  MyFinancesTests
+//
+//  Created by Rafael Ferreira on 12/15/19.
+//  Copyright © 2019 Swift Yah. All rights reserved.
+//
+
+import XCTest
+@testable import MyFinances
+
+class MyFinancesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
