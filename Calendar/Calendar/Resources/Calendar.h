a50669cac3039eaa26935c09d3a9b0c5ad22284c
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2019-12-31 09:30:20 -0300

    Add Calendar.framework

---
+++ Calendar/Calendar/Resources/Calendar.h
@@ -0,0 +1,19 @@
+//
+//  Calendar.h
+//  Calendar
+//
+//  Created by Rafael Ferreira on 12/31/19.
+//  Copyright © 2019 Swift Yah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Calendar.
+FOUNDATION_EXPORT double CalendarVersionNumber;
+
+//! Project version string for Calendar.
+FOUNDATION_EXPORT const unsigned char CalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calendar/PublicHeader.h>
+
+
