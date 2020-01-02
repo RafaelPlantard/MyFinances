101c3542955418b26c21f7185eaff9fac24634d6
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2020-01-02 16:52:36 -0300

    Add MonthCollectionViewDelegate

---
+++ Calendar/Calendar/Scenes/ListYear/Views/MonthCollectionViewDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  MonthCollectionViewDelegate.swift
+//  Calendar
+//
+//  Created by Rafael Ferreira on 12/31/19.
+//  Copyright © 2019 Swift Yah. All rights reserved.
+//
+
+import UIKit
+
+final class MonthCollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = collectionView.frame.width / 3.6
+        let width = height
+
+        return CGSize(width: width, height: height)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+}
