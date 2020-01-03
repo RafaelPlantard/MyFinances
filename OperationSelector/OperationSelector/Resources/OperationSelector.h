fbeec18001e686b1e58c73499656176de0407192
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2020-01-03 17:06:03 -0300

    Add OperationSelector.framework

---
+++ OperationSelector/OperationSelector/Resources/OperationSelector.h
@@ -0,0 +1,19 @@
+//
+//  OperationSelector.h
+//  OperationSelector
+//
+//  Created by Rafael Ferreira on 1/3/20.
+//  Copyright © 2020 Swift Yah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OperationSelector.
+FOUNDATION_EXPORT double OperationSelectorVersionNumber;
+
+//! Project version string for OperationSelector.
+FOUNDATION_EXPORT const unsigned char OperationSelectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OperationSelector/PublicHeader.h>
+
+
